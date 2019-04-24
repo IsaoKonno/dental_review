a9144b31fd3f35442de74a4e74357cf65cefa0be
KONNO <ikmnhu8_0112@yahoo.co.jp>
2019-04-24 15:35:48 +0900

    edit

--- app/controllers/glomouth_frontend/clinics_controller.rb
+++ app/controllers/glomouth_frontend/clinics_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class ::GlomouthFrontend::ClinicsController < ApplicationController
+  def index
+    @clinics = Clinic.all.limit(10).includes(:accesses)
+  end
 
+  def show
+    @clinic = Clinic.find_by(id: params[:id])
+  end
 
   def new
     @clinic_form = ::Forms::ClinicForm.pre_build
@@ -9,7 +15,25 @@ class ::GlomouthFrontend::ClinicsController < ApplicationController
 
   def create
     prefecture_id = Prefecture.first.id
-    @clinic_form  = ::Forms::ClinicForm.new(clinic_form_params.merge!(prefecture_id: prefecture_id))
+    @clinic_form  = ::Forms::ClinicForm.new(clinic_form_params.except('id').merge!(prefecture_id: prefecture_id))
+
+    if @clinic_form.save_for_clinic
+      flash[:notice] = 'Created contacts'
+      redirect_to new_glomouth_frontend_clinic_path
+    else
+      flash[:notice] = 'There were errors'
+      render action: 'new'
+    end
+  end
+
+  def edit
+    clinic_id = params[:id]
+    @clinic_form = ::Forms::ClinicForm.load(clinic_id)
+  end
+
+  def update
+    clinic = Clinic.find_by(id: params[:id])
+    @clinic_form = ::Forms::ClinicForm.new(clinic_form_params.merge!(id: clinic.id, prefecture_id: clinic.prefecture_id))
 
     if @clinic_form.save_for_clinic
       flash[:notice] = 'Created contacts'
@@ -24,8 +48,8 @@ class ::GlomouthFrontend::ClinicsController < ApplicationController
 
   def clinic_form_params
     params.require(:forms_clinic_form).permit(
-        :name, :post_code, :address, :display_flag, :how_to_online_reservation,
-        accesses_attributes: [:clinic_id, :description]
+        :id, :name, :post_code, :address, :display_flag, :how_to_online_reservation,
+        accesses_attributes: [:id, :clinic_id, :description]
     )
   end
 end
